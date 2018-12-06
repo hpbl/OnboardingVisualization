c422c874fca7d0e8f0e8dc9a3cd1a367bee9c2a7
Victor Miranda <vmm@cin.ufpe.br>
2018-12-06 01:19:44 -0300

    getting forks and pr data

--- src/15_visualize_number_prs_new_contributors.js
+++ src/15_visualize_number_prs_new_contributors.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-await-in-loop */
 
-const token = '43aae801c5a8102a0468a7ce5398fac958611e6c';
+const token = '7bcf525661ad1aa9f62d7308ba76d487ac3bbd77';
 
 async function getRepo(user, repo) {
   const fetchResult = await fetch(`https://api.github.com/repos/${user}/${repo}?access_token=${token}`);
@@ -27,15 +27,50 @@ async function getPrs(user, repo) {
   return prsList;
 }
 
-async function numberPrsNewContributorsAccepted(user, repo, callback) {
+const knownForks = [];
+
+function prIsFromFork(pr) {
+  // verifica se o pr veio de um fork deletado ou um ativo e o fork não foi contado ainda
+  const response = (pr.head.repo === null || pr.head.repo.fork === true)
+    && knownForks.indexOf(pr.head.label) === -1;
+  knownForks.push(pr.head.label);
+  return response;
+}
+
+function prFromGhostForks(pr) {
+  return pr.head.repo === null;
+}
+
+function prFromForkWithoutGhost(pr) {
+  // verifica se o pr veio de um fork ativo e o fork não foi contado ainda
+  const response = pr.head.repo != null && pr.head.repo.fork === true
+    && knownForks.indexOf(pr.head.label) === -1;
+  knownForks.push(pr.head.label);
+  return response;
+}
+
+async function forksResultedInPR(user, repo, callback) {
   const repoData = await getRepo(user, repo);
   console.log(repoData);
 
   const prsData = await getPrs(user, repo);
   console.log(prsData);
 
+  // total de forks ativos + os pr de forks deletados
+  const totalForks = repoData.forks + prsData.filter(prFromGhostForks).length;
+  console.log(`actual forks: ${totalForks}`);
+  console.log(`number of prs: ${prsData.length}`);
+  console.log(`ghost forks: ${prsData.filter(prFromGhostForks).length}`);
+  console.log(`prs from fork:${prsData.filter(prIsFromFork).length}`);
+  const numberPRsByFork = prsData.filter(prIsFromFork).length;
+
+  // dados de pr sem forks deletados
+  const totalForksWithoutGhosts = repoData.forks;
+  const numberPRsByForkWithoutGhosts = prsData.filter(prFromForkWithoutGhost).length;
+  console.log(`${numberPRsByFork}${totalForksWithoutGhosts}${numberPRsByForkWithoutGhosts}`);
   const div = '<div> visualization </div>';
   callback(div);
 }
 
-numberPrsNewContributorsAccepted('ambujraj', 'hacktoberfest2018', div => div);
+// forksResultedInPR('ambujraj', 'hacktoberfest2018', div => div);
+forksResultedInPR('CMU-Perceptual-Computing-Lab', 'openpose', div => div);
