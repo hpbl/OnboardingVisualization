2229db8da8db3981b66b3afe9ff4c3df40cf095f
hpbl <hiltonpintor@gmail.com>
2018-12-06 12:42:18 +0000

    Automated deployment to GitHub Pages: 172829aa87c9cf8a69ac07b2ac6691af9a74ba99 [ci skip]

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// Use a Node.js core library
-const d3 = require('d3');
-
-// Log the parts object to our browser's console
-d3.select('body').style('background-color', 'cyan');
-
-console.log(d3.version);
-console.log('testando');
