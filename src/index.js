b45bf2bca1e5f804a6ea022788d979b50885ea60
Hilton Pintor <hiltonpintor@gmail.com>
2018-12-06 09:19:51 -0300

    Testing parcel with multiple modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 // Use a Node.js core library
 import * as d3 from 'd3';
+import changeH1Color from './teste';
 
 // Log the parts object to our browser's console
 d3.select('body').style('background-color', 'lightblue');
 
 console.log(d3.version);
 console.log('testando');
+
+
+changeH1Color();
