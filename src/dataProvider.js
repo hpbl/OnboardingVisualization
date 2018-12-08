fb70b3e028a5e5026ce1996265f0f2c4761310b3
Renan Freitas <rfl3@cin.ufpe.br>
2018-12-08 18:17:45 -0300

    Requesting reviews for each PR

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -18,5 +18,25 @@ export async function getPrs(user, repo) {
   return prsList;
 }
 
+export async function getReviewsForPrs(user, repo) {
+  const prs = await getPrs(user, repo);
+  console.log(prs);
+  for (let i = 0; i < prs.length; i += 1) {
+    prs[i].review = [];
+    let page = 1;
+    let promiseValue = [0];
+    while (promiseValue.length !== 0) {
+      const fetchResult = await fetch(`https://api.github.com/repos/${user}/${repo}/pulls/${prs[i].number}/reviews?access_token=${token}&page=${page}&per_page=100`);
+      const promise = fetchResult.json();
+      promiseValue = await promise.then(value => value);
 
-export default { getPrs };
+      prs[i].review = prs[i].review.concat(promiseValue);
+      page += 1;
+    }
+  }
+
+  return prs;
+}
+
+
+export default { getPrs, getReviewsForPrs };
